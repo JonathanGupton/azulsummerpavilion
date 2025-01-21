6da1a5d9a6dc901aaedaa9d7e11225640c3972e0
Jonathan Gupton <14988577+JonathanGupton@users.noreply.github.com>
2025-01-21 14:49:08 -0600

    feat: implement MakeTileSelection match case

--- azulsummerpavilion/library/game.py
+++ azulsummerpavilion/library/game.py
@@ -1,10 +1,16 @@
-from typing import Optional
 from dataclasses import dataclass
-from dataclasses import field
+from typing import Optional
 
+from azulsummerpavilion.library.actions import DistributeTiles
+from azulsummerpavilion.library.actions import DistributeTilesToSupply
+from azulsummerpavilion.library.actions import FillSupplySpaces
+from azulsummerpavilion.library.actions import MakeTileSelection
+from azulsummerpavilion.library.actions import SelectTilesToDrawFromBag
+from azulsummerpavilion.library.constants import Bag
+from azulsummerpavilion.library.constants import SUPPLY_SPACE_COUNT
+from azulsummerpavilion.library.constants import Supply
 from azulsummerpavilion.library.logic import game_logic
 from azulsummerpavilion.library.player_interface import PlayerInterface
-from azulsummerpavilion.library.queue import MessageDequeue
 from azulsummerpavilion.library.queue import MessageQueue
 from azulsummerpavilion.library.random_interface import RandomInterface
 from azulsummerpavilion.library.state import AzulSummerPavilionState as State
@@ -27,10 +33,22 @@ class GameManager:
         calling the game logic to modify the game's state.
         """
         while game.actions:
-            action = game.actions.popleft()
+            aq = game.actions
+            action = aq.popleft()
             match action:
+                case MakeTileSelection(
+                    source=source, tile_count=tile_count, target=target
+                ) if isinstance(source, Bag):
+                    tiles = game.random.draw(game.state.tiles, draw_count=tile_count)
+                    aq.appendleft(
+                        DistributeTiles(tiles=tiles, source=source, target=target)
+                    )
+                case MakeTileSelection(source=source):
+                    # TODO: This will be the player decision making logic
+                    pass
                 case _:
+                    aq.appendleft(action)
                     game.state, game.actions, game.events = game_logic(
                         game.actions, game.state, game.events
                     )
-            return game
+        return game
