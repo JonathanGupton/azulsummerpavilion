128a613b8dc24080ec8197dbe80337ce2f05e3b1
Jonathan Gupton <14988577+JonathanGupton@users.noreply.github.com>
2025-01-21 15:36:08 -0600

    feat: add SetRoundAndWildColor action, refactor FillFactoryDisplaySpaces, refactor DistributeTiles logic

--- azulsummerpavilion/library/logic.py
+++ azulsummerpavilion/library/logic.py
@@ -1,18 +1,21 @@
 from azulsummerpavilion.library.actions import Action
 from azulsummerpavilion.library.actions import CompleteInitialization
-from azulsummerpavilion.library.actions import DistributeTilesToSupply
+from azulsummerpavilion.library.actions import DistributeTiles
 from azulsummerpavilion.library.actions import DoPlayerTurn
 from azulsummerpavilion.library.actions import FillFactoryDisplaySpaces
 from azulsummerpavilion.library.actions import FillSupplySpaces
+from azulsummerpavilion.library.actions import MakeTileSelection
 from azulsummerpavilion.library.actions import NewGame
-from azulsummerpavilion.library.actions import SelectTilesToDrawFromBag
 from azulsummerpavilion.library.actions import SetGamePhase
+from azulsummerpavilion.library.actions import SetRoundAndWildColor
 from azulsummerpavilion.library.actions import UpdatePlayerScore
+from azulsummerpavilion.library.constants import Bag
 from azulsummerpavilion.library.constants import FACTORY_SPACE_DRAW
 from azulsummerpavilion.library.constants import FactoryDisplay
 from azulsummerpavilion.library.constants import INITIAL_PLAYER_SCORE
 from azulsummerpavilion.library.constants import PLAYER_TO_DISPLAY_RATIO
 from azulsummerpavilion.library.constants import Phase
+from azulsummerpavilion.library.constants import Purple
 from azulsummerpavilion.library.constants import SUPPLY_SPACE_COUNT
 from azulsummerpavilion.library.constants import Supply
 from azulsummerpavilion.library.events import GamePhaseSet
@@ -37,30 +40,41 @@ def game_logic(
 
         case NewGame(number_of_players=number_of_players):
             aq.append(SetGamePhase(Phase.acquire_tile))
+            aq.append(SetRoundAndWildColor(game_round=1, wild_color=Purple()))
             for player in range(number_of_players):
                 aq.append(UpdatePlayerScore(player, INITIAL_PLAYER_SCORE))
             aq.append(FillSupplySpaces())  # Handled by GM
 
         case FillSupplySpaces():
             aq.appendleft(
-                SelectTilesToDrawFromBag(draw_count=SUPPLY_SPACE_COUNT, target=Supply())
+                MakeTileSelection(
+                    tile_count=SUPPLY_SPACE_COUNT, source=Bag(), target=Supply()
+                )
             )
 
-        case DistributeTilesToSupply():
-            # TODO:  do the distribution
-            aq.append(FillFactoryDisplaySpaces())
-
         case FillFactoryDisplaySpaces():
-            for display_index in reversed(
-                range(PLAYER_TO_DISPLAY_RATIO[state.player_count])
-            ):
-                aq.append(
-                    SelectTilesToDrawFromBag(
-                        draw_count=FACTORY_SPACE_DRAW,
+            number_of_displays = PLAYER_TO_DISPLAY_RATIO[state.player_count]
+            for display_index in reversed(range(number_of_displays)):
+                aq.appendleft(
+                    MakeTileSelection(
+                        tile_count=FACTORY_SPACE_DRAW,
+                        source=Bag(),
                         target=FactoryDisplay(display_index),
                     )
                 )
             aq.append(CompleteInitialization())
+
+        case DistributeTiles(tiles=tiles, source=source, target=target) if isinstance(
+            target, Supply
+        ):
+
+            # TODO:  do the distribution
+            aq.append(FillFactoryDisplaySpaces())
+
+        case DistributeTiles(tiles=tiles, source=source, target=target) if isinstance(
+            target, FactoryDisplay
+        ):
+            display_index = target.display_index
 
         case UpdatePlayerScore(player=player, score=score):
             state.score.update(player, score)
@@ -79,6 +93,10 @@ def game_logic(
         case SetGamePhase(phase=phase):
             state.phase = phase
             eq.append(GamePhaseSet(phase))
+
+        case SetRoundAndWildColor(game_round=game_round, wild_color=wild_color):
+            state.round = game_round
+            state.wild_color = wild_color
 
         case _:
             pass
