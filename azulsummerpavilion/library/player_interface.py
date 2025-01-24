aa279d25481757a6f0444128a7fb2fd3c6295a91
Jonathan Gupton <14988577+JonathanGupton@users.noreply.github.com>
2025-01-24 09:06:57 -0600

    refactor: rename send_action to get_action

--- azulsummerpavilion/library/player_interface.py
+++ azulsummerpavilion/library/player_interface.py
@@ -5,7 +5,6 @@ from abc import ABC
 from abc import abstractmethod
 
 from azulsummerpavilion.library.actions import Action
-from azulsummerpavilion.library.events import Event
 from azulsummerpavilion.library.player import Player
 
 
@@ -16,11 +15,6 @@ class PlayerInterface(ABC):
         self.player = player
 
     @abstractmethod
-    def send_event(self, event: Event):
-        """Send an event to the player."""
-        pass
-
-    @abstractmethod
-    def send_action(self, action: Action) -> Action:
+    def get_action(self, action: Action) -> Action:
         """Send an action (request) to the player.  The player responds with their next action."""
         pass
