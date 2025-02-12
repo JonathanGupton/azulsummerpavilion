58c9ce10bff41c3f7b91524620903564132d4639
Jonathan Gupton <14988577+JonathanGupton@users.noreply.github.com>
2025-02-11 18:02:10 -0600

    feat: add bonus_spaces and position modules

---
+++ azulsummerpavilion/library/bonus_spaces.py
@@ -0,0 +1,157 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Iterator
+from typing import Sequence
+
+from azulsummerpavilion.library.board import Board
+from azulsummerpavilion.library.constants import StarColor
+from azulsummerpavilion.library.position import BoardPosition
+
+_PILLAR_POSITIONS: Sequence[Sequence[BoardPosition]] = (
+    (
+        BoardPosition(StarColor.Orange, 2),
+        BoardPosition(StarColor.Orange, 3),
+        BoardPosition(StarColor.Wild, 6),
+        BoardPosition(StarColor.Wild, 1),
+    ),
+    (
+        BoardPosition(StarColor.Red, 2),
+        BoardPosition(StarColor.Red, 3),
+        BoardPosition(StarColor.Wild, 1),
+        BoardPosition(StarColor.Wild, 2),
+    ),
+    (
+        BoardPosition(StarColor.Blue, 2),
+        BoardPosition(StarColor.Blue, 3),
+        BoardPosition(StarColor.Wild, 2),
+        BoardPosition(StarColor.Wild, 3),
+    ),
+    (
+        BoardPosition(StarColor.Yellow, 2),
+        BoardPosition(StarColor.Yellow, 3),
+        BoardPosition(StarColor.Wild, 3),
+        BoardPosition(StarColor.Wild, 4),
+    ),
+    (
+        BoardPosition(StarColor.Green, 2),
+        BoardPosition(StarColor.Green, 3),
+        BoardPosition(StarColor.Wild, 4),
+        BoardPosition(StarColor.Wild, 5),
+    ),
+    (
+        BoardPosition(StarColor.Purple, 2),
+        BoardPosition(StarColor.Purple, 3),
+        BoardPosition(StarColor.Wild, 5),
+        BoardPosition(StarColor.Wild, 6),
+    ),
+)
+
+_STATUE_POSITIONS: Sequence[Sequence[BoardPosition]] = (
+    (
+        BoardPosition(StarColor.Orange, 1),
+        BoardPosition(StarColor.Orange, 2),
+        BoardPosition(StarColor.Red, 3),
+        BoardPosition(StarColor.Red, 4),
+    ),
+    (
+        BoardPosition(StarColor.Red, 1),
+        BoardPosition(StarColor.Red, 2),
+        BoardPosition(StarColor.Blue, 3),
+        BoardPosition(StarColor.Blue, 4),
+    ),
+    (
+        BoardPosition(StarColor.Blue, 1),
+        BoardPosition(StarColor.Blue, 2),
+        BoardPosition(StarColor.Yellow, 3),
+        BoardPosition(StarColor.Yellow, 4),
+    ),
+    (
+        BoardPosition(StarColor.Yellow, 1),
+        BoardPosition(StarColor.Yellow, 2),
+        BoardPosition(StarColor.Green, 3),
+        BoardPosition(StarColor.Green, 4),
+    ),
+    (
+        BoardPosition(StarColor.Green, 1),
+        BoardPosition(StarColor.Green, 2),
+        BoardPosition(StarColor.Purple, 3),
+        BoardPosition(StarColor.Purple, 4),
+    ),
+    (
+        BoardPosition(StarColor.Purple, 1),
+        BoardPosition(StarColor.Purple, 2),
+        BoardPosition(StarColor.Orange, 3),
+        BoardPosition(StarColor.Orange, 4),
+    ),
+)
+
+_WINDOW_POSITIONS: Sequence[Sequence[BoardPosition]] = (
+    (
+        BoardPosition(StarColor.Orange, 5),
+        BoardPosition(StarColor.Orange, 6),
+    ),
+    (
+        BoardPosition(StarColor.Red, 5),
+        BoardPosition(StarColor.Red, 6),
+    ),
+    (
+        BoardPosition(StarColor.Blue, 5),
+        BoardPosition(StarColor.Blue, 6),
+    ),
+    (
+        BoardPosition(StarColor.Yellow, 5),
+        BoardPosition(StarColor.Yellow, 6),
+    ),
+    (
+        BoardPosition(StarColor.Green, 5),
+        BoardPosition(StarColor.Green, 6),
+    ),
+    (
+        BoardPosition(StarColor.Purple, 5),
+        BoardPosition(StarColor.Purple, 6),
+    ),
+)
+
+
+@dataclass(frozen=True)
+class Pillar:
+    adjacent: tuple[int, ...]
+    draw_count: int = 1
+
+
+@dataclass(frozen=True)
+class Statue:
+    adjacent: tuple[int, ...]
+    draw_count: int = 2
+
+
+@dataclass(frozen=True)
+class Window:
+    adjacent: tuple[int, ...]
+    draw_count: int = 3
+
+
+class BonusSpace:
+    """
+    Base class to unite windows, pillars and statues bonus spaces under a
+    single API
+    """
+
+    _positions = [_PILLAR_POSITIONS, _STATUE_POSITIONS, _WINDOW_POSITIONS]
+    _bonus_space_classes = [Pillar, Statue, Window]
+
+    def __init__(self):
+        self._bonus_space: set[Pillar | Statue | Window] = set()
+        for _class_positions, _class in zip(self._positions, self._bonus_space_classes):
+            for group in _class_positions:
+                positions: tuple[int, ...] = tuple(p.flatten() for p in group)
+                self._bonus_space.add(_class(positions))
+
+    def surrounded_spaces(self, board: Board) -> Iterator[Pillar | Statue | Window]:
+        for space in self._bonus_space:
+            if all(board.board.flatten().take(space.adjacent)):
+                yield space
+
+    def remove_space(self, space: Pillar | Statue | Window):
+        self._bonus_space.remove(space)
---
+++ azulsummerpavilion/library/position.py
@@ -0,0 +1,65 @@
+"""Module containing classes for referencing board and draw positions"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from azulsummerpavilion.library.constants import StarColor
+from azulsummerpavilion.library.constants import TileIndex
+from azulsummerpavilion.library.constants import TileTarget
+from azulsummerpavilion.library.tile_array import TileArray
+
+
+@dataclass(frozen=True, order=True)
+class BoardPosition:
+    star: StarColor
+    tile_value: int
+
+    def flatten(self) -> int:
+        """Convert the 2D (star, tile_value) to the 1D position on the board"""
+        return self.star * 6 + self.tile_value - 1
+
+    def __iter__(self):
+        yield self.star
+        yield self.tile_value
+
+    def __str__(self):
+        return f"(star={self.star.name}, value={self.tile_value})"
+
+
+@dataclass(frozen=True)
+class DrawPosition:
+    location: TileIndex
+    tiles_position: int = 0
+    tiles: TileArray = TileArray([0, 0, 0, 0, 0, 0])
+
+    def __str__(self):
+        return f"{self.location.name}-{self.tiles_position} - {str(self.tiles)}"
+
+    def as_tile_position(self) -> TilePosition:
+        return TilePosition(
+            getattr(TileTarget, self.location.name), self.tiles_position
+        )
+
+
+@dataclass
+class TilePosition:
+    """Dataclass for specifying the location to play a tile to or from which
+    to retrieve a tile
+
+    TilePosition is used as an argument in the logic modules
+    """
+
+    location: TileTarget
+    nth: int = 0
+
+    def __str__(self):
+        if self.location in {
+            TileTarget.Bag,
+            TileTarget.Tower,
+            TileTarget.TableCenter,
+            TileTarget.Supply,
+        }:
+            return str(self.location.value)
+        else:
+            return f"{str(self.location.value)}-{self.nth}"
