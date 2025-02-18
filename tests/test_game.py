618708bccc9a4fa54a19d7faf6c34bde9d0d7667
Jonathan Gupton <14988577+JonathanGupton@users.noreply.github.com>
2025-02-18 13:57:45 -0600

    test: group Game class tests into TestGame class

--- tests/test_game.py
+++ tests/test_game.py
@@ -8,40 +8,39 @@ from conftest import mock_player
 from conftest import mock_random
 
 
-def test_game_initialization(mock_player, mock_random):
-    # When
-    game = AzulSummerPavilionGame.new_game(
-        players=(mock_player, mock_player), random=mock_random
-    )
+class TestGame:
+    def test_game_initialization(self, mock_player, mock_random):
+        # When
+        game = AzulSummerPavilionGame.new_game(
+            players=(mock_player, mock_player), random=mock_random
+        )
 
-    # Then
-    assert isinstance(game.actions, MessageQueue)
-    assert isinstance(game.events, MessageQueue)
-    assert game.state is None
-    assert len(game.actions) == 1
-    assert isinstance(game.actions.popleft(), NewGame)
+        # Then
+        assert isinstance(game.actions, MessageQueue)
+        assert isinstance(game.events, MessageQueue)
+        assert game.state is None
+        assert len(game.actions) == 1
+        assert isinstance(game.actions.popleft(), NewGame)
 
+    @pytest.mark.parametrize("player_count", [2, 3, 4])
+    def test_game_initialization_multiple_players(self, player_count, mock_random):
+        # Given
+        players = tuple(MockPlayer() for _ in range(player_count))
 
-@pytest.mark.parametrize("player_count", [2, 3, 4])
-def test_game_initialization_multiple_players(player_count, mock_random):
-    # Given
-    players = tuple(MockPlayer() for _ in range(player_count))
+        # When
+        game = AzulSummerPavilionGame.new_game(players=players, random=mock_random)
 
-    # When
-    game = AzulSummerPavilionGame.new_game(players=players, random=mock_random)
+        # Then
+        assert len(game.players) == player_count
+        new_game_action = game.actions.popleft()
+        assert isinstance(new_game_action, NewGame)
+        assert new_game_action.number_of_players == player_count
 
-    # Then
-    assert len(game.players) == player_count
-    new_game_action = game.actions.popleft()
-    assert isinstance(new_game_action, NewGame)
-    assert new_game_action.number_of_players == player_count
+    @pytest.mark.parametrize("player_count", [0, 5])
+    def test_game_initialization_fails_with_invalid_player_counts(
+        self, player_count, mock_random
+    ):
+        players = tuple(MockPlayer() for _ in range(player_count))
 
-
-@pytest.mark.parametrize("player_count", [0, 5])
-def test_game_initialization_fails_with_invalid_player_counts(
-    player_count, mock_random
-):
-    players = tuple(MockPlayer() for _ in range(player_count))
-
-    with pytest.raises(ValueError):
-        AzulSummerPavilionGame.new_game(players=players, random=mock_random)
+        with pytest.raises(ValueError):
+            AzulSummerPavilionGame.new_game(players=players, random=mock_random)
